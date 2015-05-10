68dab2abe9254f4b7bb69b8cd1ff894bd766e039
Ovidiu PODARIU <ocpodariu@yahoo.com>
2015-05-11 00:00:44 +0300

    DepartmentController now handles all types of HTTP requests

--- src/main/java/ro/teamnet/zth/app/controller/DepartmentController.java
+++ src/main/java/ro/teamnet/zth/app/controller/DepartmentController.java
@@ -1,26 +1,52 @@
 package ro.teamnet.zth.app.controller;
 
-import ro.teamnet.zth.api.annotations.MyController;
-import ro.teamnet.zth.api.annotations.MyRequestMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 import ro.teamnet.zth.app.domain.Department;
 import ro.teamnet.zth.app.service.DepartmentService;
 import ro.teamnet.zth.app.service.DepartmentServiceImpl;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Author: Ovidiu
  * Date:   5/6/2015
  */
-@MyController(urlPath = "/departments")
+@Controller
+@RequestMapping(value = "/departments")
 public class DepartmentController {
 
     private DepartmentService departmentService = new DepartmentServiceImpl();
 
-    @MyRequestMethod(urlPath = "/all")
-    public List<Department> getAllDepartments() {
+    @RequestMapping(method = RequestMethod.GET)
+    public @ResponseBody List<Department> getAllDepartments() {
         return departmentService.findAllDepartments();
+    }
+
+    @RequestMapping(value = "/{idDepartment}", method = RequestMethod.GET)
+    public @ResponseBody Department getOneDepartment(@PathVariable("idDepartment") String idDepartment) {
+        Integer id = Integer.valueOf(idDepartment);
+        return departmentService.findOneDepartment(id);
+    }
+
+    @RequestMapping(value = "/{idDepartment}", method = RequestMethod.DELETE)
+    @ResponseStatus(HttpStatus.OK)
+    public void delete(@PathVariable("idDepartment") String idDepartment) {
+        Integer id = Integer.valueOf(idDepartment);
+        departmentService.deleteDepartment(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.OK)
+    public void insert(@RequestBody Department department) {
+        departmentService.insertDepartment(department);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT)
+    @ResponseStatus(HttpStatus.OK)
+    public void update(@RequestBody Department department) {
+        departmentService.updateDepartment(department);
     }
 
 }
