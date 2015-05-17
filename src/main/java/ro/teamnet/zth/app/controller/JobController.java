069ba5779a8e821abdbc8c07b37d36f93f6fd5f3
Ovidiu PODARIU <ocpodariu@yahoo.com>
2015-05-17 19:10:17 +0300

    JobController now handles all types of HTTP requests

--- src/main/java/ro/teamnet/zth/app/controller/JobController.java
+++ src/main/java/ro/teamnet/zth/app/controller/JobController.java
@@ -1,7 +1,8 @@
 package ro.teamnet.zth.app.controller;
 
-import ro.teamnet.zth.api.annotations.MyController;
-import ro.teamnet.zth.api.annotations.MyRequestMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 import ro.teamnet.zth.app.domain.Job;
 import ro.teamnet.zth.app.service.JobService;
 import ro.teamnet.zth.app.service.JobServiceImpl;
@@ -12,19 +13,41 @@ import java.util.List;
  * Author: Ovidiu
  * Date:   5/6/2015
  */
-@MyController(urlPath = "/jobs")
+@Controller
+@RequestMapping(value = "/jobs")
 public class JobController {
 
     private JobService jobService = new JobServiceImpl();
 
-    @MyRequestMethod(urlPath = "/all")
-    public List<Job> getAllJobs() {
+    @RequestMapping(method = RequestMethod.GET)
+    public @ResponseBody List<Job> getAllJobs() {
         return jobService.findAllJobs();
     }
 
-    @MyRequestMethod(urlPath = "/one")
-    public String getOneJob() {
-        return "oneRandomJob";
+    @RequestMapping(method = RequestMethod.GET, value = "/{idJob}")
+    public @ResponseBody Job getOneJob(@PathVariable("idJob") String idJob) {
+        Integer id = Integer.valueOf(idJob);
+        return jobService.findOneJob(id);
     }
+
+    @RequestMapping(value = "/{idJob}", method = RequestMethod.DELETE)
+    @ResponseStatus(HttpStatus.OK)
+    public void delete(@PathVariable("idJob") String idJob) {
+        Integer id = Integer.valueOf(idJob);
+        jobService.deleteJob(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.OK)
+    public void insert(@RequestBody Job job) {
+        jobService.insertJob(job);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT)
+    @ResponseStatus(HttpStatus.OK)
+    public void update(@RequestBody Job job) {
+        jobService.updateJob(job);
+    }
+
 
 }
